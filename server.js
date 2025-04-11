98191fba0743acfe1e8f5e19a97d7256b59a9106
Dmytro Bobdar <db@siur.org.ua>
2025-04-12 01:35:54 +0300

    fix name

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import http from 'http';
 
 const requestHandler = (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('<h1>This is DB site</h1>');
+  res.end('<h1>This is Dmytryi Bondar site</h1>');
 };
 
 http.createServer(requestHandler).listen(80, () => {
